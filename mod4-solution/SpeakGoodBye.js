5813530a84eca2a82b2b2de58ab69d7218ee9bac
Ayla202 <97741925+Ayla202@users.noreply.github.com>
2022-02-11 21:30:43 +0100

    Update SpeakGoodBye.js

--- mod4-solution/SpeakGoodBye.js
+++ mod4-solution/SpeakGoodBye.js
@@ -3,7 +3,6 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
-
 (function (window) {
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
@@ -15,15 +14,17 @@
 var speakWord = "Good Bye";
 var byeSpeaker = { speak: function(name) {
   console.log(speakWord + " " + name);
-  
+
 }}
+
+
+
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-
-window.byeSpeaker = byeSpeaker;
+window.byeSpeaker = byeSpeaker
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
-}}(window);
+})(window);
